7ec3379e549b3fedd8c69e7db5e6536b3fac7525
tahar belghitri <tahartlm0@gmail.com>
2023-02-01 22:03:16 +0100

    Update theme.ts

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -1,4 +1,35 @@
 import { extendTheme } from "@chakra-ui/react";
 
 // theme config
-export default extendTheme({});
+export default extendTheme({
+  direction: "rtl",
+
+  styles: {
+    global: {
+      body: {
+        p: "0px",
+        m: "0px",
+        color: "#EEEEEE",
+        bg: "#1B1B1B",
+        fontSize: "18px",
+        fontFamily: "'El Messiri', sans-serif;",
+      },
+    },
+  },
+  colors: {
+    black: {
+      0: "#262626",
+    },
+  },
+  shadows: {
+    black: "0 0 10px rgb(0,0,0,25%)",
+  },
+  breakpoints: {
+    start: "0px",
+    sm: "320px",
+    md: "768px",
+    lg: "960px",
+    xl: "1200px",
+    "2xl": "1536px",
+  },
+});
