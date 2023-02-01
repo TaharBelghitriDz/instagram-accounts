d5139d4aed7c591da84a244d9688cd4ec878f03b
tahar belghitri <tahartlm0@gmail.com>
2023-02-01 22:03:05 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,36 @@
-import { Text } from "@chakra-ui/react";
+import { HStack, Image, Text } from "@chakra-ui/react";
 import { useState } from "react";
+import Loading, { ProgressLoading } from "./components/loading";
+import Login from "./components/login/login";
+import RightSideBar from "./components/right.sideBar";
 
 function App() {
   const [count, setCount] = useState();
 
-  return <Text> dashboard </Text>;
+  const location = window.location.pathname;
+
+  // if (location == "/login") return <Login />;
+
+  return (
+    <HStack w="full" justifyContent="space-between" spacing="0">
+      <Image
+        pos="absolute"
+        top="0"
+        left="0"
+        zIndex={-1}
+        src="/back.svg"
+        h="100vh"
+        objectFit="cover"
+      />
+      <Login />
+      {/* <Text>ني يتيت يتي تي يت</Text>
+      <Login />
+      <Loading isLoading={true} />
+      <RightSideBar /> */}
+      {/* <ProgressLoading />
+      <Loading isLoading={false} /> */}
+    </HStack>
+  );
 }
 
 export default App;
