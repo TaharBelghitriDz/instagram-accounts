3ceda933e41517e9d53af448812cd2a40f73ec1e
tahar belghitri <tahartlm0@gmail.com>
2023-02-03 02:25:31 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,43 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image, Stack, Text } from "@chakra-ui/react";
 import { useState } from "react";
 import Loading, { ProgressLoading } from "./components/loading";
 import Login from "./components/login/login";
 import RightSideBar from "./components/right.sideBar";
+import Pages from "./pages";
 
 function App() {
   const [count, setCount] = useState();
 
   const location = window.location.pathname;
 
-  // if (location == "/login") return <Login />;
+  if (location == "/login") return <Login />;
 
   return (
-    <HStack w="full" justifyContent="space-between" spacing="0">
-      <Image
-        pos="absolute"
-        top="0"
-        left="0"
-        zIndex={-1}
-        src="/back.svg"
-        h="100vh"
-        objectFit="cover"
-      />
-      <Login />
-      {/* <Text>ني يتيت يتي تي يت</Text>
-      <Login />
-      <Loading isLoading={true} />
-      <RightSideBar /> */}
-      {/* <ProgressLoading />
+    <Stack w="full" alignItems="center" justifyContent="center">
+      <HStack
+        w="full"
+        maxW="1250px"
+        justifyContent="space-between"
+        spacing="0"
+        p="15px"
+        alignItems="start"
+      >
+        <Image
+          pos="absolute"
+          top="0"
+          left="0"
+          zIndex={-1}
+          src="/back.svg"
+          h="100vh"
+          objectFit="cover"
+        />
+
+        <RightSideBar />
+        <Pages place="/" />
+        {/* <ProgressLoading />
       <Loading isLoading={false} /> */}
-    </HStack>
+      </HStack>
+    </Stack>
   );
 }
 
