d99bbcff07a770aedcae0647274e104145c9b14a
tahar belghitri <tahartlm0@gmail.com>
2023-02-03 02:26:02 +0100

    Update login.tsx

--- client/src/components/login/login.tsx
+++ client/src/components/login/login.tsx
@@ -1,4 +1,4 @@
-import { HStack, VStack } from "@chakra-ui/react";
+import { HStack, Image, VStack } from "@chakra-ui/react";
 import LoginInputsComponent from "./login.inputs.component";
 import LoginMobileComponent from "./login.mobile.component";
 import LoginSideComponent from "./login.side.component";
@@ -13,6 +13,15 @@ export default () => {
       // bg="rgb(0,0,0,10%)"
       // backdropFilter="blur(5px)"
     >
+      <Image
+        pos="absolute"
+        top="0"
+        left="0"
+        zIndex={-1}
+        src="/back.svg"
+        h="100vh"
+        objectFit="cover"
+      />
       <LoginSideComponent />
       <LoginMobileComponent />
 
