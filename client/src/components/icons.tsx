81783df9d92a4da144235c4c8d36cf8b91096f29
tahar belghitri <tahartlm0@gmail.com>
2023-02-03 02:25:55 +0100

    icons

--- client/src/components/icons.tsx
+++ client/src/components/icons.tsx
@@ -5,3 +5,50 @@ export const UserIcon = createIcon({
   viewBox: "0 0 24 24",
   d: "M5 20h14v2H5v-2zm7-2a8 8 0 110-16 8 8 0 010 16z",
 });
+
+export const Historiq = createIcon({
+  displayName: "Historiq",
+  viewBox: "0 0 24 24",
+  d: "M3.087 9h17.826a1 1 0 01.997 1.083l-.834 10a1 1 0 01-.996.917H3.92a1 1 0 01-.996-.917l-.834-10A1 1 0 013.087 9zM4.84 19h14.32l.666-8H4.174l.666 8zm8.574-14H20a1 1 0 011 1v1H3V4a1 1 0 011-1h7.414l2 2z",
+});
+
+export const Proxy = createIcon({
+  displayName: "Proxy",
+  viewBox: "0 0 24 24",
+  d: "M7.105 8.79A3.001 3.001 0 0010 11h4a5.001 5.001 0 014.927 4.146 3.001 3.001 0 11-2.84 5.166 3 3 0 01.808-5.102A3.001 3.001 0 0014 13h-4a4.978 4.978 0 01-3-1v3.17a3.001 3.001 0 11-2 0V8.83a3.001 3.001 0 112.105-.04z",
+});
+
+export const List = createIcon({
+  displayName: "List",
+  viewBox: "0 0 24 24",
+  d: "M8 4h13v2H8V4zM4.5 6.5a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm0 7a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm0 6.9a1.5 1.5 0 110-3 1.5 1.5 0 010 3zM8 11h13v2H8v-2zm0 7h13v2H8v-2z",
+});
+
+export const Pen = createIcon({
+  displayName: "Pen",
+  viewBox: "0 0 24 24",
+  d: "M7.243 18H3v-4.243L14.435 2.322a1 1 0 011.414 0l2.829 2.829a1 1 0 010 1.414L7.243 18zM3 20h18v2H3v-2z",
+});
+
+export const Add = createIcon({
+  displayName: "Add",
+  viewBox: "0 0 21 21",
+  d: "M9.667 9.667v-5h1.666v5h5v1.666h-5v5H9.667v-5h-5V9.667h5z",
+});
+
+export const Down = createIcon({
+  displayName: "Down",
+  viewBox: "0 0 24 24",
+  d: "M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222l4.95 4.95z",
+});
+
+export const Refresh = createIcon({
+  displayName: "Refresh",
+  viewBox: "0 0 24 24",
+  d: "M5.463 4.433A9.961 9.961 0 0112 2c5.523 0 10 4.477 10 10 0 2.136-.67 4.116-1.81 5.74L17 12h3A8 8 0 006.46 6.228l-.997-1.795zm13.074 15.134A9.962 9.962 0 0112 22C6.477 22 2 17.523 2 12c0-2.136.67-4.116 1.81-5.74L7 12H4a8 8 0 0013.54 5.772l.997 1.795z",
+});
+export const Settings = createIcon({
+  displayName: "Settings",
+  viewBox: "0 0 24 24",
+  d: "M12 1l9.5 5.5v11L12 23l-9.5-5.5v-11L12 1zm0 2.311L4.5 7.653v8.694l7.5 4.342 7.5-4.342V7.653L12 3.311zM12 16a4 4 0 110-8 4 4 0 010 8zm0-2a2 2 0 100-4 2 2 0 000 4z",
+});
