67e3a6904ef206e614ecde5834544f48ccacbdc4
tahar belghitri <tahartlm0@gmail.com>
2023-02-06 02:25:47 +0100

    Update index.tsx

--- client/src/components/lists/lists.body.components/index.tsx
+++ client/src/components/lists/lists.body.components/index.tsx
@@ -1,13 +1,18 @@
-import { VStack } from "@chakra-ui/react";
+import { Text, VStack } from "@chakra-ui/react";
 import state from "../../../utils/state";
 
 import ListsBodyNameComponent from "./lists.body.name.component";
 
 export default () => {
   const place = state.useStore((e) => e.listPlace);
+
+  let Comp = ListsBodyNameComponent;
+  if (place == "names") Comp = ListsBodyNameComponent;
+  else Comp = () => <Text>no</Text>;
+
   return (
     <VStack spacing="0px" w="full" bg="#323232" rounded="20px" p="20px">
-      <ListsBodyNameComponent />
+      <Comp />
     </VStack>
   );
 };
