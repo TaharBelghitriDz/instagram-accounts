8bf6772c22e0ff4a39a1e8493898dcd230818c6b
tahar belghitri <tahartlm0@gmail.com>
2023-03-12 01:44:35 +0100

    Update accounts.table.component.tsx

--- client/src/components/account/tables/accounts.table.component.tsx
+++ client/src/components/account/tables/accounts.table.component.tsx
@@ -1,4 +1,5 @@
 import {
+  Checkbox,
   Image,
   Table,
   TableCaption,
@@ -16,9 +17,14 @@ import { Account, accountGet } from "../../../utils/api/accounts.api";
 import state from "../../../utils/state";
 import Models from "../models";
 import AccountsDetails from "../models/accounts.details";
+import RemoveAccounts from "../models/remove.accounts";
 import AccountsTableHeaderComponent from "./accounts.table.header.component";
 
-const Row = (props: { onClick: (e: any) => void }) => {
+const Row = (props: {
+  selected: number[];
+  onClick: (e: any) => void;
+  onCheckBox: (e: number) => void;
+}) => {
   const accountsState: Account[] = state.useStore((e) => e.accounts);
   const [accounts, setAccounts] = useState<Account[]>(accountsState);
   const selectedGroup = state.useStore((e) => e.selectedGroup);
@@ -35,18 +41,19 @@ const Row = (props: { onClick: (e: any) => void }) => {
     });
   }, [selectedGroup]);
 
-  console.log(accounts);
-
   return (
     <Tbody>
       {accounts?.map((e, i) => (
-        <Tr
-          key={i * 12}
-          onClick={() => {
-            props.onClick(e.id);
-          }}
-        >
+        <Tr key={i * 12} bg={props.selected.includes(e.id) ? "red.800" : ""}>
           <Td>
+            <Checkbox
+              checked={props.selected.includes(e.id)}
+              onChange={({ target: { checked } }) => {
+                props.onCheckBox(e.id);
+              }}
+            />
+          </Td>
+          <Td onClick={() => props.onCheckBox(e.id)}>
             <Image
               src={e.profile_pic}
               h="75px"
@@ -55,14 +62,18 @@ const Row = (props: { onClick: (e: any) => void }) => {
               rounded="10px"
             />
           </Td>
-          <Td textAlign="center">{e.username}</Td>
-          <Td textAlign="center"> {e.is_active ? "مفعل" : "غير مفعل"} </Td>
-          <Td textAlign="center">
+          <Td onClick={() => props.onClick(e.id)} textAlign="center">
+            {e.username}
+          </Td>
+          <Td onClick={() => props.onClick(e.id)} textAlign="center">
+            {e.is_active ? "مفعل" : "غير مفعل"}{" "}
+          </Td>
+          <Td onClick={() => props.onClick(e.id)} textAlign="center">
             <Text p="10px" rounded="10px" bg="green.900" color="green.100">
               {e.status}
             </Text>
           </Td>
-          <Td isNumeric>
+          <Td onClick={() => props.onClick(e.id)} isNumeric>
             <VStack>
               <span>{e.created_date.slice(0, 12)}</span>
               <span>{e.created_date.slice(13, 23)}</span>
@@ -76,7 +87,24 @@ const Row = (props: { onClick: (e: any) => void }) => {
 
 export default () => {
   const discloser = useDisclosure();
-  const [selctedAccount, setSelectedAccount] = useState(100);
+  const [selctedAccount, setSelectedAccount] = useState(0);
+  // const [selectedCheckBox, setselectedCheckBox] = useState<number[]>([]);
+
+  const selectedCheckBox = state.useStore((e) => e.selectedAccounts);
+
+  const setselectedCheckBox = (e: number[]) =>
+    state.changeState({ selectedAccounts: e });
+
+  const CheckBox = (id: number) => {
+    // console.log(selectedCheckBox);
+
+    if (!selectedCheckBox.includes(id))
+      return setselectedCheckBox([...selectedCheckBox, id]);
+    else
+      return setselectedCheckBox(
+        selectedCheckBox.filter((e: any) => e !== id && e)
+      );
+  };
 
   return (
     <VStack
@@ -94,6 +122,7 @@ export default () => {
         content={<AccountsDetails {...discloser} id={selctedAccount} />}
         {...discloser}
       />
+
       <Table
         variant="simple"
         colorScheme="whiteAlpha"
@@ -124,10 +153,14 @@ export default () => {
         </Thead>
         <Row
           {...discloser}
+          selected={selectedCheckBox}
           onClick={(e: any) => {
             setSelectedAccount(() => e);
 
             discloser.onOpen();
+          }}
+          onCheckBox={(id: number) => {
+            CheckBox(id);
           }}
         />
       </Table>
